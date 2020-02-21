e7e19651473baf939063c6d4e8bf270ec5bb2076
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-02-21 16:25:24 +0100

    square working!

--- main.py
+++ main.py
@@ -1,77 +1,101 @@
 import curses
 
+MODE = 1
+MODES = ['SQUARE', 'LINE', 'MANUAL']
+
+
 canva = list()
 x, y = 0, 0
 
 
-def add_line_up():
-    pass
-
-
-def add_line_down():
-    canva.append([' '] * len(canva[0]))
-    pass
-
-
-def add_column_left():
-    pass
-
-
-def add_column_right():
-    pass
+class point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 def draw(stdscr):
+    global x, y
     stdscr.addstr(0, 0, 'ascii-drawer by Fran6nd.' + str(x) + ' ' + str(y))
     stdscr.addstr(
-        1, 0, 'Press [tab] so switch beween edit mode and screen mode.')
+        1, 0, 'Press [KEY_F1] so switch beween edit modes (' + MODES[MODE] + ' selected)')
     height, width = stdscr.getmaxyx()
     height -= 2
     start_x = int(x - width/2)
     start_y = int(y - height/2)
+
     for _x in range(0, width-1):
         for _y in range(0, height-1):
             if start_x + _x == x and start_y + _y == y:
                 stdscr.addstr(
-                    _y+2, _x, canva[start_x + _x][start_y + height + 1 -_y], curses.color_pair(2))
+                    _y+2, _x, canva[start_x + _x][start_y + height + 1 - _y], curses.color_pair(2))
             else:
                 stdscr.addstr(
-                    _y+2, _x, canva[start_x + _x][start_y + height + 1 -_y], curses.color_pair(1))
-    stdscr.refresh()
-
+                    _y+2, _x, canva[start_x + _x][start_y + height + 1 - _y], curses.color_pair(1))
 
 def main(stdscr):
     # Clear screen
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
     height, width = stdscr.getmaxyx()
-    for i in range(0, width+10):
-        canva.append([' '] * (height+10))
+    for i in range(0, width*10):
+        canva.append([' '] * (height*10))
     global x
     global y
     x = int(len(canva)/2)
     y = int(len(canva[0])/2)
     c = 0
+    p1 = None
+    p2 = None
     while chr(c) != 'q':
         stdscr.clear()
         draw(stdscr)
+        stdscr.refresh()
 
         stdscr.refresh()
         c = stdscr.getch()
         if c == curses.KEY_UP:
             y = y + 1
-            add_line_up()
         elif c == curses.KEY_DOWN:
             y = y - 1
-            add_line_down()
         elif c == curses.KEY_RIGHT:
             x = x + 1
-            add_column_right()
         elif c == curses.KEY_LEFT:
             x = x - 1
-            add_column_left()
+        elif c == 9:
+            p1 = None
+            p2 = None
+            global MODE
+            MODE += 1
+            if MODE == len(MODES):
+                MODE = 0
         else:
-            canva[x][y] = chr(c)
+            if(MODES[MODE] == 'SQUARE'):
+                if(c == 32):
+                    if p1 == None:
+                        p1 = point(x, y)
+                    else:
+                        p2 = point(x, y)
+                        x_min = min(p1.x, p2.x)
+                        x_max = max(p1.x, p2.x)
+                        y_min = min(p1.y, p2.y)
+                        y_max = max(p1.y, p2.y)
+                        for _x in range(x_min, x_max):
+                            canva[_x][y_min] = '-'
+                        for _x in range(x_min, x_max):
+                            canva[_x][y_max] = '-'
+                        for _y in range(y_min, y_max):
+                            canva[x_min][_y] = '|'
+                        for _y in range(y_min, y_max):
+                            canva[x_max][_y] = '|'
+                        canva[x_min][y_min] = '+'
+                        canva[x_min][y_max] = '+'
+                        canva[x_max][y_min] = '+'
+                        canva[x_max][y_max] = '+'
+                        p1 = None
+                        p2 = None
+            else:
+                canva[x][y] = chr(c)
 
 
 curses.wrapper(main)
