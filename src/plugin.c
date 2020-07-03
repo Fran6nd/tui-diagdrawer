0dcafc1322d36372f1c42198daeb16570376ecb2
François NOUAILLE DEGORCE <nouaille.degorce.francois@gmail.com>
2020-07-03 16:04:09 +0200

    src/plugin.c: improve free system.

--- src/plugin.c
+++ src/plugin.c
@@ -7,44 +7,54 @@
 
 #include "plugin.h"
 
-struct plugin{
-    lua_State *plugin;
-    char* name; 
+struct plugin {
+  lua_State *plugin;
+  char *name;
 };
 
-int plugin_load(char* path) {
+int plugin_load(char *path) {
+  if (plugins.loaded == 0) {
+    plugins.loaded++;
+    plugins.plugins = (lua_State **)malloc(sizeof(lua_State *));
+  } else {
+    plugins.loaded++;
+    plugins.plugins = (lua_State **)realloc(
+        plugins.plugins, sizeof(lua_State *) * plugins.loaded);
+  }
   int status, result, i;
   double sum;
-  lua_State *L;
 
-  L = luaL_newstate();
+  plugins.plugins[plugins.loaded-1] = luaL_newstate();
 
-  luaL_openlibs(L);
+  luaL_openlibs(plugins.plugins[plugins.loaded-1]);
 
-  status = luaL_loadfile(L, path);
+  status = luaL_loadfile(plugins.plugins[plugins.loaded-1], path);
   if (status) {
-    fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(L, -1));
+    fprintf(stderr, "Couldn't load file: %s\n", lua_tostring(plugins.plugins[plugins.loaded-1], -1));
     exit(1);
   }
 
   /* Ask Lua to run our little script */
-  result = lua_pcall(L, 0, LUA_MULTRET, 0);
+  result = lua_pcall(plugins.plugins[plugins.loaded-1], 0, LUA_MULTRET, 0);
   if (result) {
-    fprintf(stderr, "Failed to run script: %s\n", lua_tostring(L, -1));
+    fprintf(stderr, "Failed to run script: %s\n", lua_tostring(plugins.plugins[plugins.loaded-1], -1));
     exit(1);
   }
 }
-void plugins_load(){
-    plugins.loaded = 0;
-    plugin_load("lua/custom_mode.lua");
+void plugins_load() {
+  plugins.loaded = 0;
+  plugin_load("lua/custom_mode.lua");
 }
-void plugins_free(){
-    int i;
-    for(i = 0; i < plugins.loaded; i++){
-        lua_close(plugins.plugins[i]);
+void plugins_free() {
+  int i;
+  if (plugins.loaded != 0) {
+    for (i = 0; i < plugins.loaded; i++) {
+      lua_close(plugins.plugins[i]);
     }
+    free(plugins.plugins);
+  }
 }
-const char* plugin_get_name(lua_State *L){
+const char *plugin_get_name(lua_State *L) {
   int t = lua_getglobal(L, "NAME");
-  return lua_tostring (L,1);
+  return lua_tostring(L, 1);
 }
