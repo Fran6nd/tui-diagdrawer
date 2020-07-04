f0e60c6e4ffd3b89c6f3bfc57f626ec9d7bc8320
François NOUAILLE DEGORCE <nouaille.degorce.francois@gmail.com>
2020-07-05 01:02:25 +0200

    include/edit_mode.h: improve comments and cleanup!

--- include/edit_mode.h
+++ include/edit_mode.h
@@ -5,16 +5,26 @@
 #include "main.h"
 
 typedef struct edit_mode {
+  /* The name of the edit_mode. */
+  char *name;
+  /* The key used to access the edit_mode. */
+  int key;
+  /* Function used to manage events. */
   void (*on_key_event)(int);
+  /* Function used to hook character drawing. */
   character (*on_draw)(position, character);
+  /* These two functions are called when we move all characters to left or down
+   * by adding a line on top or a column on the left side.
+   * They are used to tell the edit_mode that everything moved. */
   void (*on_top_line_add)();
   void (*on_left_column_add)();
+  /* Called when exiting the current mode. */
   void (*on_exit)();
+  /* Called when the app is closing. */
   void (*on_free)();
+  /* Used to abort the current task.
+   * Return 1 if a task has been aborted, else 0. */
   int (*on_abort)();
-  char *name;
-  int key;
-  char *help;
 } edit_mode;
 
 extern struct edit_mode *modes;
