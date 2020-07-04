700e61a5107901c5abba8fdb6a381dcd35741780
François NOUAILLE DEGORCE <nouaille.degorce.francois@gmail.com>
2020-07-05 01:02:46 +0200

    src/edit_mode.c: improve comments.

--- src/edit_mode.c
+++ src/edit_mode.c
@@ -1,18 +1,21 @@
-#include <stdlib.h>
 #include <curses.h>
+#include <stdlib.h>
 
 #include "edit_mode.h"
 #include "ui.h"
 
-
 struct edit_mode *modes;
 int edit_mode_counter;
+
+/* Here we add an edit_mode after checking if there is no conflicts with the key
+ * used. */
 void register_mode(edit_mode em) {
   int i;
-  for(i = 0; i < edit_mode_counter; i++){
-    if(modes[i].key == em.key){
-      ui_show_text("Error\nTrying to register an edit mode\n using an already used key.");
-      getch();
+  for (i = 0; i < edit_mode_counter; i++) {
+    if (modes[i].key == em.key) {
+      ui_show_text_info(
+          "Error\nTrying to register an edit mode\n using an already "
+          "used key.");
       return;
     }
   }
@@ -27,6 +30,7 @@ void register_mode(edit_mode em) {
   edit_mode_counter++;
 }
 
+/* Here we register all edit modes that are gonna be availables. */
 int register_modes() {
   register_mode(rect_mode());
   register_mode(put_mode());
@@ -38,6 +42,7 @@ int register_modes() {
 }
 
 char *get_menu() {
+  /* #TODO: build the menu dynamically from registered edit modes. */
   return ("You are in the SELECT mode.\n"
           "You have selected one rect. Here is what you can do:\n"
           "      [space] to deselect.\n"
@@ -50,6 +55,8 @@ char *get_menu() {
           "\n"
           "Press any key to continue.");
 }
+
+/* Return if found the edit mode using the key given as parameter. */
 edit_mode *get_edit_mode(int key) {
   int i = 0;
   for (; i < edit_mode_counter; i++) {
@@ -60,11 +67,13 @@ edit_mode *get_edit_mode(int key) {
   return NULL;
 }
 
-void edit_mode_free() { 
+/* Called at when the app is closing... */
+void edit_mode_free() {
   int i;
-  for(i = 0; i < edit_mode_counter; i++){
-    if(modes[i].on_free != NULL){
+  for (i = 0; i < edit_mode_counter; i++) {
+    if (modes[i].on_free != NULL) {
       modes[i].on_free();
     }
   }
-  free(modes); }
+  free(modes);
+}
