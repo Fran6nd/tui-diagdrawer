d617fbe98122e5da56e0a883a64f288985440734
François NOUAILLE DEGORCE <nouaille.degorce.francois@gmail.com>
2020-07-09 13:37:07 +0200

    src/demo_plugin.lua: improvements.

--- src/demo_plugin.lua
+++ src/demo_plugin.lua
@@ -5,6 +5,10 @@ function get_help()
     return "I'm the demo plugin!"
 end
 
+p = {}
+p.x = 1
+p.y = 1
+
 function on_key_event(key)
     if move_cursor(key) == nil then
         --show_message_blocking("Ok! working! " .. get_cursor_pos().x .. " " .. get_cursor_pos().y .. " " .. (key or "(nil)"))
@@ -12,16 +16,13 @@ function on_key_event(key)
         --p.y = p.y + 1
         --set_cursor_pos(p)
         show_message_blocking(get_char_at(get_cursor_pos()))
-        set_char_at(get_cursor_pos(), nil)
+        set_char_at(get_cursor_pos(), string.byte("x"))
+        p = get_cursor_pos()
     end
 end
 function on_draw(pos, char)
-    
-    if(pos.x % 2 == 0)then
-        char.color = COL_EMPTY
-    end
-    if(pos.y % 2 == 0)then
-        char.color = COL_EMPTY
+    if(pos.x == p.x and pos.y == p.y)then
+        char.color = COL_SELECTION
     end
     return char
 end
