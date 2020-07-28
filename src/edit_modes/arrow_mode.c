05718ee68a26bffe7a37aad25c88bdc0e5974205
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-07-28 09:23:35 +0200

    arrow_mode.c: fix ctrl + arrow behaviour.

--- src/edit_modes/arrow_mode.c
+++ src/edit_modes/arrow_mode.c
@@ -9,33 +9,92 @@
 
 position_list PATH;
 
-static void on_key_event(edit_mode *em, int c) {
-  if (!move_cursor(c).null) {
-    if (PATH.size != 0) {
-      position tmp = get_cursor_pos();
-      while (pl_is_inside(&PATH, tmp) != -1) {
-        pl_remove_last(&PATH);
+static void on_key_event(edit_mode *em, int c)
+{
+  position delta = move_cursor(c);
+  if (!delta.null)
+  {
+    if (PATH.size != 0)
+    {
+      position previous_pos = {.x = get_cursor_pos().x - delta.x, .y = get_cursor_pos().y - delta.y, .null = 0};
+      position p_min = pos_min(previous_pos, get_cursor_pos());
+      position p_max = pos_max(previous_pos, get_cursor_pos());
+      delta.x = -delta.x;
+      delta.y = -delta.y;
+      while (delta.x < 0)
+      {
+        delta.x++;
+        position tmp = get_cursor_pos();
+        tmp.x = tmp.x + delta.x;
+
+        while (pl_is_inside(&PATH, tmp) != -1)
+        {
+          pl_remove_last(&PATH);
+        }
+        pl_add(&PATH, tmp);
       }
-      pl_add(&PATH, tmp);
+      while (delta.y < 0)
+      {
+        delta.y++;
+        position tmp = get_cursor_pos();
+        tmp.y = tmp.y + delta.y;
+
+        while (pl_is_inside(&PATH, tmp) != -1)
+        {
+          pl_remove_last(&PATH);
+        }
+        pl_add(&PATH, tmp);
+      }
+      while (delta.x > 0)
+      {
+        delta.x--;
+        position tmp = get_cursor_pos();
+        tmp.x = tmp.x + delta.x;
+
+        while (pl_is_inside(&PATH, tmp) != -1)
+        {
+          pl_remove_last(&PATH);
+        }
+        pl_add(&PATH, tmp);
+      }
+      while (delta.y > 0)
+      {
+        delta.y--;
+        position tmp = get_cursor_pos();
+        tmp.y = tmp.y + delta.y;
+
+        while (pl_is_inside(&PATH, tmp) != -1)
+        {
+          pl_remove_last(&PATH);
+        }
+        pl_add(&PATH, tmp);
+      }
     }
-  } else if (c == ' ') {
-    if (PATH.size != 0) {
+  }
+  else if (c == ' ')
+  {
+    if (PATH.size != 0)
+    {
       int i;
       do_change(&CURRENT_FILE);
-      for (i = 0; i < PATH.size; i++) {
+      for (i = 0; i < PATH.size; i++)
+      {
 
         chk_set_char_at(&CURRENT_FILE, PATH.list[i],
                         pl_get_arrow_char(&PATH, i));
       }
       pl_empty(&PATH);
       PATH = pl_new();
-    } else {
+    }
+    else
+    {
       pl_add(&PATH, get_cursor_pos());
     }
   }
 }
 
-static char *get_help(edit_mode *em) {
+static char *get_help(edit_mode *em)
+{
   return "You are in the ARROW mode.\n"
          "You can use [space] to select the starting point\n"
          "and then [space] again to select the ending point\n"
@@ -46,36 +105,45 @@ static char *get_help(edit_mode *em) {
 
 static void on_free(edit_mode *em) { pl_empty(&PATH); }
 
-static character on_draw(edit_mode *em, position p, character c) {
+static character on_draw(edit_mode *em, position p, character c)
+{
   int i = pl_is_inside(&PATH, p);
-  if (i != -1) {
+  if (i != -1)
+  {
     c.c = pl_get_arrow_char(&PATH, i);
   }
   return c;
 }
 
-static void on_left_column_add(edit_mode *em) {
+static void on_left_column_add(edit_mode *em)
+{
   int i = 0;
-  for (i = 0; i < PATH.size; i++) {
+  for (i = 0; i < PATH.size; i++)
+  {
     PATH.list[i].x++;
   }
 }
-static void on_top_line_add(edit_mode *em) {
+static void on_top_line_add(edit_mode *em)
+{
   int i = 0;
-  for (i = 0; i < PATH.size; i++) {
+  for (i = 0; i < PATH.size; i++)
+  {
     PATH.list[i].y++;
   }
 }
 
-static int on_abort(edit_mode *em) {
-  if (PATH.size != 0) {
+static int on_abort(edit_mode *em)
+{
+  if (PATH.size != 0)
+  {
     pl_empty(&PATH);
     return 1;
   }
   return 0;
 }
 
-edit_mode arrow_mode() {
+edit_mode arrow_mode()
+{
   edit_mode EDIT_MODE_RECT = {.name = "ARROW",
                               .key = (int)'a',
                               .on_key_event = on_key_event,
